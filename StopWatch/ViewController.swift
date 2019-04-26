6a313c843b47b5059378cf679a9f2fa824f37e13
Nick <plus0bit@gmail.com>
2019-04-26 21:06:09 +0300

    Initial xcode project

---
+++ StopWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StopWatch
+//
+//  Created by Nick on 26/04/2019.
+//  Copyright © 2019 Nick Danilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
